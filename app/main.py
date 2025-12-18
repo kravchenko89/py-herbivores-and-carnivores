d1e59d983ac53fa3674f74b5bb800c0b432eb4e0
kravchenko89 <vadymkravchenko.us@gmail.com>
2025-12-18 17:48:32 -0500

    Solution

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 class Animal:
     alive = []
 
-    def __init__(self, name: str, health: int | float = 100, hidden = False) -> None:
+    def __init__(self, name: str, health=100, hidden=False) -> None:
         self.name = name
         self.health = health
         self.hidden = hidden
